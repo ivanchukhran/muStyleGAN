4bf776385efd9b17e9e5ce04bda066235417213d
Ivan Chukhran <ivan.chuhran@gmail.com>
2023-05-07 21:48:09 +0300

    add train loop

---
+++ train.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+import torchvision.transforms as transforms
+
+def epoch():
+    pass
+
+def train_loop():
+    pass
+
