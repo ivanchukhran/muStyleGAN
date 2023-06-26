25ac8f37e32a0cd16f13585fbfe4a3e290572956
Ivan Chukhran <ivan.chuhran@gmail.com>
2023-06-26 19:38:43 +0300

    implement cli interface for model training, add sample and plots saving functionality, refactor

--- train.py
+++ train.py
@@ -1,6 +1,5 @@
+import click
 import datetime
-import os
-from typing import Optional
 
 from torch.optim import Optimizer, Adam
 from torch.utils.data import DataLoader
@@ -26,6 +25,8 @@ def train_loop(
         device: str = "cuda",
         display_step: Optional[int] = None,
         lazy_gradient_penalty: int = 4,
+        plot_dir: Optional[str] = None,
+        sample_dir: Optional[str] = None
 ) -> None:
     cur_step = 0
 
@@ -51,11 +52,7 @@ def train_loop(
                 disc_fake_pred = discriminator(fake.detach())
                 disc_real_pred = discriminator(real)
 
-                # epsilon = torch.rand(len(real), 1, 1, 1, device=device, requires_grad=True)
-                # gradient = gradient_of(discriminator, real, fake.detach(), epsilon)
-
                 disc_loss = discriminator_loss(disc_real_pred, disc_fake_pred)
-                # print(f"disc_loss: {disc_loss.shape}")
                 if cur_step % lazy_gradient_penalty == 0:
                     gp = gradient_penalty(real, disc_real_pred)
                     disc_loss += gp * c_lambda
@@ -89,8 +86,8 @@ def train_loop(
                     gen_mean = sum(generator_losses[-display_step:]) / display_step
                     crit_mean = sum(critic_losses[-display_step:]) / display_step
                     print(f"Epoch {epoch}, step {cur_step}: Generator loss: {gen_mean}, critic loss: {crit_mean}")
-                    show_tensor_images(fake)
-                    show_tensor_images(real)
+                    show_tensor_images(fake, save_path=f"{sample_dir}/fake_sample_{epoch}.png")
+                    show_tensor_images(real, save_path=f"{sample_dir}/real_sample_{epoch}.png")
                     step_bins = 20
                     num_examples = (len(generator_losses) // step_bins) * step_bins
                     plt.plot(
@@ -103,22 +100,47 @@ def train_loop(
                         torch.Tensor(critic_losses[:num_examples]).view(-1, step_bins).mean(1),
                         label="Discriminator Loss"
                     )
-                    # print(f"Gen losses: {len(generator_losses)}, Critic losses: {len(critic_losses)}")
-                    # plt.plot(generator_losses, label=f"Generator Loss")
-                    # plt.plot(critic_losses, label=f"Discriminator Loss")
                     plt.legend()
                     plt.show()
+                    if plot_dir:
+                        plt.savefig(f"{plot_dir}/plot_{epoch}.png")
+                    plt.close()
             cur_step += 1
 
 
-def train(epochs: int, dry_run: bool = False, kaggle_mode: bool = False):
+@click.command()
+@click.option('--num_epochs', default=100_000, help='Number of epochs to train the model for. '
+                                                    '100_000 is the default.')
+@click.option('--mode', default='local', help='Mode to run the training in.'
+                                              '`local` is the default.'
+                                              '`local` will run the training locally, '
+                                              '`kaggle` supposed to run on Kaggle.com platform')
+@click.option('--resolution', default=32, help='Resolution of the images to train on. 32 is the default.')
+@click.option('--display-step', default=500, help='Number of steps to display the images for. The 500 is the default.'
+                                                  'If none is given, it will not display the images.')
+@click.option('--save-step', default=500, help='Number of steps to save the images for. 500 is the default.')
+@click.option('--batch-size', default=8, help='Batch size to use for training. 8 is the default.')
+@click.option('--dry-run', default=False, help='Whether to do a dry run of the training. False is the default.')
+@click.option('--plot-dir', default=None, help='Directory to save the plots in. '
+                                               'None is the default. '
+                                               'If none is given, it will not save the plots.')
+@click.option('--sample-dir', default=None, help='Directory to save the samples in.'
+                                                 'None is the default. '
+                                                 'If none is given, it will not save the samples.')
+def train(
+        num_epochs: int = 100_000,
+        mode: str = "local",
+        resolution: int = 32,
+        display_step: int = 500,
+        save_step: int = 500,
+        batch_size: int = 8,
+        dry_run: bool = False,
+        plot_dir: Optional[str] = None,
+        sample_dir: Optional[str] = None
+) -> None:
     z_dim = 128
     w_dim = 256
     n_mapping_layers = 5
-    image_resolution = 32
-
-    display_step = 500
-    batch_size = 8
 
     synthesis_lr = 2e-5
     synthesis_betas = (0.5, 0.9)
@@ -131,23 +153,36 @@ def train(epochs: int, dry_run: bool = False, kaggle_mode: bool = False):
 
     c_lambda = 10
     crit_repeats = 5
-    n_epochs = epochs
+    n_epochs = num_epochs
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    save_step = 500
-    formatted_date = datetime.datetime.now().strftime("%d%m%y")
     save_path = f"weights"
+    formatted_date = datetime.datetime.now().strftime("%d%m%y")
+    dir_version = f"v{len(filter_by_dirname(save_path, formatted_date)) + 1}"
 
-    if similar_dir_exists(save_path, formatted_date):
-        save_path = f"{save_path}/" \
-                    f"{formatted_date}" \
-                    f"v{len([folder for folder in os.listdir('weights') if formatted_date in folder]) + 1}"
+    save_path = os.path.join(save_path, formatted_date, dir_version)
+
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
     else:
-        save_path = f"{save_path}/{formatted_date}v1"
+        print(f"Directory {save_path} already exists.")
 
-    create_dir(save_path)
+    if plot_dir is not None:
+        plot_dir = os.path.join(plot_dir, formatted_date, dir_version)
+        create_dir_or_ignore(plot_dir)
 
-    dataset_path = "data/landscapes" if not kaggle_mode else "../input/landscapes/landscapes"  # Need to be changed
+    if sample_dir is not None:
+        sample_dir = os.path.join(sample_dir, formatted_date, dir_version)
+        create_dir_or_ignore(sample_dir)
+
+    match mode:
+        case "local":
+            dataset_path = "data/landscapes"
+        case "kaggle":
+            dataset_path = "../input/landscapes/landscapes"
+        case _:
+            raise ValueError(f"Unknown mode: {mode}")
+
     dataset = Dataset(dataset_path)
 
     dataloader = DataLoader(
@@ -156,14 +191,13 @@ def train(epochs: int, dry_run: bool = False, kaggle_mode: bool = False):
         shuffle=True,
         num_workers=2
     )
-
     gen = Generator(
         z_dim=z_dim,
         w_dim=w_dim,
-        image_resolution=image_resolution,
+        image_resolution=resolution,
         n_mapping_layers=n_mapping_layers
     ).to(device)
-    disc = Discriminator(resolution=image_resolution).to(device)
+    disc = Discriminator(resolution=resolution).to(device)
 
     gen_opt = Adam([
         {'params': gen.synthesis.parameters(), 'lr': synthesis_lr, 'betas': synthesis_betas},
@@ -189,7 +223,9 @@ def train(epochs: int, dry_run: bool = False, kaggle_mode: bool = False):
         device=device,
         save_step=save_step,
         save_dir=save_path,
-        display_step=display_step
+        display_step=display_step,
+        plot_dir=plot_dir,
+        sample_dir=sample_dir
     )
 
 
@@ -200,4 +236,4 @@ def main() -> None:
 
 
 if __name__ == '__main__':
-    main()
+    train()
