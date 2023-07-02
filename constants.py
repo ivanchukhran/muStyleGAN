300bc5615de7f92cc7f88482bd25bb751ad6a9b2
Ivan Chukhran <ivan.chuhran@gmail.com>
2023-07-02 10:42:58 +0300

    add constants

---
+++ constants.py
@@ -0,0 +1,6 @@
+# URL: str = "https://drive.google.com/file/d/1QUBT6kZJU0R5NrzcX1WUQA9tt7Aq3-91/view?usp=sharing"
+URL: str = "https://www.kaggle.com/datasets/ueberf/sentinel-5k-truecolor/download"
+DATA_PATH: str = "data/landscapes"
+PLOT_PATH: str = "plots"
+SAMPLE_PATH: str = "samples"
+WEIGHTS_PATH: str = "weights"
